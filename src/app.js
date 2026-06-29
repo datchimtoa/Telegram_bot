8b6abef9510b7e420b8143dd9962abc38e42e4e8
datchimtoa <nguvcl688@gmail.com>
2026-06-29 23:16:58 +0700

    Initial project structure setup

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// Điểm khởi đầu
+// Main entry point for the Telegram bot application
+
+const logger = require('./logger');
+
+logger.info('Telegram Bot Application Started');
---
+++ src/logger.js
@@ -0,0 +1,16 @@
+// Winston logger
+// Centralized logging configuration
+
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
+
+module.exports = logger;
